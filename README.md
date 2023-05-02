d229151175aa872e8bd090ebefe9b4fb57002e47
parthasmcomp <parthasmcomp@gmail.com>
2023-05-03 00:11:01 +0530

    added src files, readme, apppreview

---
+++ README.md
@@ -0,0 +1,18 @@
+## README
+
+Project created as part of course https://www.udemy.com/course/react-tutorial-and-projects-course.
+
+Project showcases a simple navbar with responsive design.
+
+Made in: ReactJS
+
+### STEPS
+
+1. Download the files
+2. Run "npm install"
+3. npm run dev (project created with Vite)
+4. Navigate to http://localhost:5173/
+
+### APP PREVIEW
+
+![Birthday Buddy App Preview](https://github.com/parthamcomp/navbar-project/blob/main/AppPreview.png?raw=true)
---
+++ src/Navbar.jsx
@@ -0,0 +1,61 @@
+import { useState, useRef } from 'react'
+import { FaBars } from 'react-icons/fa'
+import logo from './logo.svg'
+import { links, social } from './data'
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false)
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
+
+  const toggleLinks = () => {
+    console.log()
+    setShowLinks(!showLinks)
+  }
+
+  const linkStyles = {
+    height: showLinks
+      ? `${linksRef.current.getBoundingClientRect().height}px`
+      : '0px',
+  }
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="logo" alt="logo" />
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+        <div
+          className="links-container"
+          ref={linksContainerRef}
+          style={linkStyles}
+        >
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map((socialLink) => {
+            const { id, url, icon } = socialLink
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </nav>
+  )
+}
+export default Navbar
